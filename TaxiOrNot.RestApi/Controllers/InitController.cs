d2b6bc1c40fbe8fabcbcdbbddaf894c4614b3dc1
Doncho Minkov <donchominkov@gmail.com>
2013-11-21 12:13:54 +0200

    Added Init Controller

---
+++ TaxiOrNot.RestApi/Controllers/InitController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace TaxiOrNot.RestApi.Controllers
+{
+    public class InitController:BaseApiController
+    {
+        public string Get()
+        {
+            return this.ExecuteOperationAndHandleException(() =>
+            {
+                return "Server is up and running!";
+            });
+        }
+    }
+}
